49f7af2eaf11ab435cc633468182b6e1d98403ea
Konstantin von Wendt <konstantin.vonwendt@innogames.com>
2023-02-03 20:41:46 +0100

    character can double jump

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
-@export var jump_speed: int = -100
-@export var gravity: int = 10
+@export var jump_speed: int = -400
+@export var gravity: int = 1000
+@export var extra_jumps: int = 2
 
+var current_jumps: int = 0
 
 func _physics_process(delta) -> void:
 	velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_speed
+	move_and_slide()
+
+	if is_on_floor():
+		current_jumps = 0
+
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
+		elif current_jumps < extra_jumps:
+			velocity.y = jump_speed
+			current_jumps += 1
