d121df84297d2012f83b42950064100ed3c3eb88
Brizzlefizz <dstammerjohannnde@gmail.com>
2023-02-03 23:23:57 +0100

    Intro Scene

---
+++ scenes/intro.gd
@@ -0,0 +1,87 @@
+extends CanvasLayer
+@onready var speech_label : Label = $MarginContainer/Panel/MarginContainer/SpeechLabel
+@onready var text_box_node : MarginContainer = $MarginContainer
+@onready var quest_giver : Sprite2D = $Dogquest
+@onready var potato : Sprite2D = $Potato
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+var text_scrolling = false
+var text = ""
+var text_percentage = 0
+var move_questgiver = false
+var questgiver_target = Vector2(87, 130)
+var potato_target = Vector2(350, 130)
+var speed = 200
+var texts = ["Hello there y'all! I need sum' helpin' with 'dem there beets!", "But make sure ta harvest to da beat! Don't break my tractor!", "Pawsome!"]
+var current_text = 0
+@onready var text_timer = $TextTimer
+enum states {SLIDE_IN, DISPLAY_TEXT, WAIT}
+var current_state = states.SLIDE_IN
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	quest_giver.set_global_position(Vector2(-100, 130))
+	potato.set_global_position(Vector2(570, 130))
+	_hide_text()
+	start()
+	
+func start():
+	_play_scene()
+
+func _play_scene():
+	if current_state == states.WAIT:
+		return
+	if current_state == states.SLIDE_IN:
+		_slide_in_quest_giver()
+	if current_state == states.DISPLAY_TEXT and not text_scrolling:
+		print("render text triggered")
+		_render_text()
+
+func _hide_text():
+	speech_label.set_text("")
+	text_box_node.hide()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if text_scrolling:
+		var percentage_visible = text_timer.time_left / (text.length() / 20.0)
+		speech_label.set_visible_ratio((1 -percentage_visible))
+
+	if move_questgiver:
+		quest_giver.set_global_position(quest_giver.global_position.move_toward(questgiver_target, delta * speed))
+		potato.set_global_position(potato.global_position.move_toward(potato_target, delta * speed))
+		if quest_giver.global_position == questgiver_target:
+			move_questgiver = false
+			current_state = states.DISPLAY_TEXT
+			_play_scene()
+	
+	if Input.is_action_just_pressed("jump") and current_state == states.WAIT:
+		current_text = current_text + 1
+		speech_label.set_visible_ratio(0)
+		if current_text == texts.size():
+			print("Dialog Finished, starting game!")
+			return
+		current_state = states.DISPLAY_TEXT
+		_play_scene()
+
+func _render_text():
+	text_box_node.show()
+	text = texts[current_text]
+	text_scrolling = true
+	speech_label.set_text(text)
+	text_timer.set_wait_time(text.length() / 20.0)
+	text_timer.start()
+	if current_text == 2:
+		animation_player.play("talk_potato")
+	else:
+		animation_player.play("talk")
+	
+func _slide_in_quest_giver():
+	move_questgiver = true
+
+func _on_text_timer_timeout():
+	text_timer.stop()
+	text_scrolling = false
+	current_state = states.WAIT
+	_play_scene()
+	animation_player.stop()
+	speech_label.set_visible_ratio(1)
