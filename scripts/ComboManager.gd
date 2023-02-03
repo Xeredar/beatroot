da9af4b8591505e122fbd55cb971bf95ca8f6899
Seref Bülbül <bulbulseref@gmail.com>
2023-02-03 23:38:43 +0100

    Fix ComboManager

--- scripts/ComboManager.gd
+++ scripts/ComboManager.gd
@@ -12,23 +12,4 @@ func missTheBeat():
 	totalMissedBeats += 1
 	currentComboCount = 0
 	totalBeats += 1
-
-func _ready():
-	var beatManager = get_node("/root/Node2D/BeatManager")
-	beatManager.beat_grace_start.connect(_on_beat_grace_start)
-	beatManager.beat_grace_end.connect(_on_beat_grace_end)
-#	var missedBeatLine = get_node("/root/Node2D/MissedBeatLine")
-#	missedBeatLine.body_entered.connect(_on_body_entered)
-	
-func _on_beat_grace_start():
-	#print("_on_beat_grace_start")
-	pass
-	
-func _on_beat_grace_end():
-	#print("_on_beat_grace_end")
-	pass
-	
-func _on_body_entered(body_rid, body, body_shape_index, local_shape_index):
-	print("_on_body_entered")
-	
 	
