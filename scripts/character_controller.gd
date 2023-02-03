11f2b66a07debd5b79ce169284e1152b8be3c6cc
Konstantin von Wendt <konstantin.vonwendt@innogames.com>
2023-02-03 20:18:15 +0100

    scenes folder and beginnings of character controller

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,11 +1,11 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var jump_speed: int = -100
+@export var gravity: int = 10
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta) -> void:
+	velocity.y += gravity * delta
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
