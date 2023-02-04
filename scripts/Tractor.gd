2cc06e81dfb48821dbbadde2e53fbff07246df35
DareLooks <the_zefan@yahoo.com>
2023-02-04 16:21:08 +0100

    exchanged graphics, added tractor view scene

---
+++ scripts/Tractor.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var _tractor : AnimationPlayer = $TractorAnimationPlayer
+@onready var _vacuum : AnimationPlayer = $VacuumAnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func set_speed(speed):
+	_tractor.speed_scale = speed
+
+func pick_beet():
+	_vacuum.play("suck")
