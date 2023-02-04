b85616a89d250376499b3a25bbc8b80d83f52fbc
Nils Daumann <nils_daumann@slindev.com>
2023-02-04 02:44:49 +0100

    Added some first points and multiplier UI

--- scripts/ComboManager.gd
+++ scripts/ComboManager.gd
@@ -1,15 +1,20 @@
 extends Node
 
 var currentComboCount = 0
+var currentMultiplier = 1
 var totalMissedBeats = 0
 var totalBeats = 0
+var totalPoints = 0
 
 func hitTheBeat():
 	currentComboCount += 1
 	totalBeats += 1
+	totalPoints += currentMultiplier
+	currentMultiplier = min(1 + floor(currentComboCount * 0.5), 8)
+
 
 func missTheBeat():
 	totalMissedBeats += 1
 	currentComboCount = 0
 	totalBeats += 1
-
+	currentMultiplier = 1
