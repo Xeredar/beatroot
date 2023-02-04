874a3b1a21b0d2962eaa1604bbfe5d910549b6c6
DareLooks <the_zefan@yahoo.com>
2023-02-05 00:03:37 +0100

    multiplier label

--- scripts/MultiplierLabel.gd
+++ scripts/MultiplierLabel.gd
@@ -1,11 +1,53 @@
-extends Label
+extends Node2D
+
+@onready var _label : Label = $Label
+@onready var _shine : Node2D = $MultiplierBgShine
+@onready var _shine2 : Node2D = $MultiplierBgShine2
+@onready var _animationPlayer : AnimationPlayer = $AnimationPlayer
+
+var _previousMultiplier : int = 1
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_animate_shine(_shine, 1, 1, 0.85, 6, 2)
+	_animate_shine(_shine2, -1, 0.87, 0.98, 8, 2.5)
+	_shine.hide()
+	_shine2.hide()
+	
+
+func _animate_shine(shine, direction, scaleA, scaleB, rotation_duration, fade_duration):
+	var rotation_tween = create_tween().set_loops()
+	rotation_tween.tween_property(shine, "rotation_degrees", 0, 0)
+	rotation_tween.tween_property(shine, "rotation_degrees", direction * 360, rotation_duration)
+	
+	var scale_tween = create_tween().set_loops()
+	scale_tween.tween_property(shine, "scale", Vector2(scaleA, scaleA), fade_duration)
+	var c1 = shine.modulate
+	var c2 = Color(c1.r, c1.g, c1.b, 0.2)
+	scale_tween.parallel().tween_property(shine, "modulate", c2, fade_duration)
+	scale_tween.tween_property(shine, "scale", Vector2(scaleB, scaleB), fade_duration)
+	scale_tween.parallel().tween_property(shine, "modulate", c1, fade_duration)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	text = "Mulitplier: {multiplier}x".format({"multiplier": ComboManager.currentMultiplier})
+	var newMultiplier = ComboManager.currentMultiplier
+	
+	if (newMultiplier == _previousMultiplier):
+		return
+		
+	_label.text = "{multiplier}x".format({"multiplier": ComboManager.currentMultiplier})
+	
+	if newMultiplier < _previousMultiplier:
+		_animationPlayer.play("decrease")
+	else:
+		_animationPlayer.play("increase")
+		
+	if newMultiplier >= 8:
+		_shine.show()
+		_shine2.show()
+	else:
+		_shine.hide()
+		_shine2.hide()
+	
+	_previousMultiplier = newMultiplier
+
