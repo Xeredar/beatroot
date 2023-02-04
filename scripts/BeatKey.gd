3a2c57c5f2c781b413dc6d636d6da8d9bf06a4a3
Seref Bülbül <bulbulseref@gmail.com>
2023-02-04 13:46:11 +0100

    Set font for BeatKey

--- scripts/BeatKey.gd
+++ scripts/BeatKey.gd
@@ -3,9 +3,12 @@ extends Label
 var speed : float
 var isActive = true
 
+@onready var font = load("res://sprites/kongtext.ttf")
+
 func _ready():
 	z_index = 2
 	scale = Vector2(1.0, 1.0)
+	add_theme_font_override("font", font)
 
 
 func _process(delta):
